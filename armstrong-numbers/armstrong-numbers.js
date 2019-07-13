3d9cfa9a5a9b6aae6f7f6f9d6c6ed5da7ad70eaf
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-13 13:39:47 +0100

    removed comment and add one test

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -1,14 +1,9 @@
-//
-// This is only a SKELETON file for the 'Armstrong numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const validate = (number) => {
   if(typeof number  !== 'number'){
-    throw new Error('You did not typed in a number')
+    throw new Error('You did not typed in a number!')
   }
     let result = [...number+'']
     .map(n => +n)
-    .reduce((sum, digit) => sum + Math.pow(digit, [...number+''].length));
+    .reduce((sum, digit) => sum + Math.pow(digit, [...number+''].length), 0);
     return result === number?true:false
 };
