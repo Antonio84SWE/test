da37b35e4312c8a1d053883ca20cfd59bf6fe21e
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-13 18:39:43 +0100

    Added two test to verify the input argument

--- two-fer/two-fer.spec.js
+++ two-fer/two-fer.spec.js
@@ -1,15 +1,24 @@
 import { twoFer } from './two-fer'
 
 describe('twoFer()', () => {
+  test('return error if a non-string is used as argument', () => {
+    const input = true
+    expect(() => twoFer(input)).toThrow(new Error('Invalid input data'));
+  })
+  test('return error if string with non-alphabetic character is used as argument', () => {
+    expect(() => twoFer('Urban2')).toThrow(new Error('Invalid input data'));
+    expect(() => twoFer('Bru465no')).toThrow(new Error('Invalid input data'));
+    expect(() => twoFer('01&Storbulle')).toThrow(new Error('Invalid input data'));
+  })
   test('no name given', () => {
     expect(twoFer()).toEqual("One for you, one for me.")
   })
 
-  xtest('a name given', () => {
+  test('a name given', () => {
     expect(twoFer("Alice")).toEqual("One for Alice, one for me.")
   })
 
-  xtest('another name given', () => {
+  test('another name given', () => {
     expect(twoFer("Bob")).toEqual("One for Bob, one for me.")
   })
 })
