baa843db9be29c86e9aa5b98f60101f37b829364
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-13 12:33:48 +0100

    'update'

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -3,6 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-export const validate = () => {
-  throw new Error("Remove this statement and implement this function");
+export const validate = (number) => {
+  if(typeof number  !== 'number'){
+    throw new Error('You did not typed in a number')
+  }
+    let result = [...number+'']
+    .map(n => +n)
+    .reduce((sum, digit) => sum + Math.pow(digit, [...number+''].length));
+    return result === number?true:false
 };
