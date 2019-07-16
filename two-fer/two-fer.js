3ade83d16b0f3530828eca19e2d59b68d284622a
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-16 21:09:55 +0100

    refactoring

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,9 +1,6 @@
- export function twoFer(name) {
-    if(name === undefined){
-       return "One for you, one for me.";
-    }
-    if(typeof name !== 'string' || name.match(/[^A-Z]/gi)){
-      throw new Error('Invalid input data');
+ export function twoFer(name = 'you') {
+    if (typeof name !== 'string' || name.match(/[^A-Z]/gi)) {
+       throw new Error('Invalid input data');
     }
     return `One for ${name}, one for me.`;
  }
