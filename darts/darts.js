55de1dad1e030f9caa493c5851af6b023a9e91f9
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-16 23:29:22 +0100

    3rd refactoring

--- darts/darts.js
+++ darts/darts.js
@@ -1,5 +1,5 @@
 export const solve = (x, y) => {
-  const d = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  const d = Math.sqrt(x*x + y*y);
   return (
     (d <= 10 && d > 5) ? 1 :
     (d <= 5 && d > 1) ? 5 :
