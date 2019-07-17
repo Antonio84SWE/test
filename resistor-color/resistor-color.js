1a3434e9eecabe606210be25a512c7918703fb3b
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-17 20:51:32 +0100

    1st refactoring

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,7 +1,3 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 const resistMnemonics = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 export const colorCode = (colorInput) => {
   return resistMnemonics.indexOf(colorInput);
