582fb20e3a35ab96940c9cf315b65763c1a5e198
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-20 23:05:59 +0100

    1st solution

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,8 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-export const value = () => {
-  
+const resistMnemonics = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+export const value = (colorCodes) => {
+  return +parseInt(colorCodes.reduce((sum, color) => {
+    return "" + sum + resistMnemonics.indexOf(color);
+  },0));
 };
+// "" forces integers into string to concatenate them
+// + trims the leading zero after the concatenation 
