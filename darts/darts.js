787afdb2f4dff8e04fa2e69e70fb21186f8efd58
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-23 12:44:07 +0100

    update

--- darts/darts.js
+++ darts/darts.js
@@ -1,7 +1,10 @@
 export const solve = (x, y) => {
+  if(typeof x !== 'number' || typeof y !== 'number'){
+    throw new Error('Both coordinates must be numbers!')
+  }
   const d = Math.sqrt(x * x + y * y);
   return (
-    (d <= 1) ? 10 :
-    (d <= 5) ? 5 :
-    (d <= 10) ? 1 : 0)
+    d <= 1 ? 10 :
+    d <= 5 ? 5 :
+    d <= 10 ? 1 : 0)
 }
