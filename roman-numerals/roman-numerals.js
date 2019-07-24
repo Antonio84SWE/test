203ecd4ed84ac0b8ca23f321fee24bb5a6191caf
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-24 23:00:01 +0100

    1st solution

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,39 +1,26 @@
-//
-// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-const romanNumber = [['I', 'X', 'C', 'M'], ['V', 'L', 'D']];
+const romanSign = [
+  ['I', 'X', 'C', 'M'],
+  ['V', 'L', 'D']
+];
 
-const romanCond = (n, i) => {
-  console.log('n & index', n ,i)
-  if(n < 4){
-    console.log('roman',romanNumber[0][i].repeat(n))
-    return romanNumber[0][i].repeat(n);
+const romanNumber = (n, index) => {
+  if (n < 4) {
+    return romanSign[0][index].repeat(n);
   }
-  if(n === 4){
-    console.log('roman',romanNumber[0][i] + romanNumber[1][i])
-    return romanNumber[0][i] + romanNumber[1][i];
-  }/*
-  if(n === 5){
-    return romanNumber[1][i];
+  if (n === 4) {
+    return romanSign[0][index] + romanSign[1][index];
   }
-  if(n > 5 && n <= 8){
-    return romanNumber[1][i] + romanNumber[0][i];
+  if (n >= 5 && n <= 8) {
+    return romanSign[1][index] + romanSign[0][index].repeat(n - 5);
   }
-  return romanNumber[0][i] + romanNumber[1][i];
-  */
+  return romanSign[0][index] + romanSign[0][index + 1];
 }
+
 export const toRoman = (number) => {
-  console.log([...number+''].map(n => +n));
- let result = [...number+'']
- .map(n => +n)
- .reverse()
- .reduce((sum, n, index) => {
-   console.log('index',index)
-   console.log('n',n)
-   return sum + romanCond(n, index);
-   console.log('reduce loop', sum + romanCond(n, index))
- },'')
- console.log('result', result)
- return result;
+  return [...number + '']
+    .map(n => +n)
+    .reverse()
+    .reduce((sum, number, index) => {
+      return romanNumber(number, index) + sum;
+    }, '')
 };
