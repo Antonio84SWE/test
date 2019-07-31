e04343957f2688037cb666ed051e4686822cd2d9
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-31 14:30:10 +0100

    1st solution

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = (number) => {
+  if (number <= 0) {
+    throw new Error('Only positive numbers are allowed');
+  }
+  return number === 1 ? 0 : stepCount(number);
 };
+
+const stepCount = n => {
+  let steps = 0;
+  while (n !== 1) {
+    n % 2 === 0 ? n /= 2 : n = 3 * n + 1;
+    steps++;
+  }
+  return steps;
+}
