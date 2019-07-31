8a9d36a6ca760c1fe1ae3c73360a39f0ad958c6f
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-07-31 14:59:16 +0100

    1st refactoring

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,15 +1,11 @@
-export const steps = (number) => {
-  if (number <= 0) {
+export const steps = (n) => {
+  if (n <= 0) {
     throw new Error('Only positive numbers are allowed');
   }
-  return number === 1 ? 0 : stepCount(number);
-};
-
-const stepCount = n => {
   let steps = 0;
   while (n !== 1) {
     n % 2 === 0 ? n /= 2 : n = 3 * n + 1;
     steps++;
   }
   return steps;
-}
+};
