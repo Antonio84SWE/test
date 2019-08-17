5b26c63bb636f52500a636202c0402409526a743
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-08-17 22:51:28 +0100

    Refactoring with template literals

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,7 @@
 const resistMnemonics = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 export const value = (colorCodes) => {
-  return +parseInt(colorCodes.reduce((sum, color) => {
-    return "" + sum + resistMnemonics.indexOf(color);
-  },0));
+  return +(colorCodes.reduce((sum, color) => 
+    `${sum}${resistMnemonics.indexOf(color)}`,0));
 };
 // "" forces integers into string to concatenate them
 // + trims the leading zero after the concatenation 
