b63a0740a6047411a93dc3fa04fa33576ce53422
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-08-19 18:06:24 +0100

    solution with added test

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -2,7 +2,16 @@
 // This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const TRANSCRIPTION = {
+  A: 'U',
+  T: 'A',
+  C: 'G',
+  G: 'C',
+}
 
-export const toRna = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const toRna = (DNA) => {
+  if(/[^ATCG]/gi.test(DNA)){
+    throw new Error('This DNA-sequence includes non-existing DNA-bases!');
+  }
+  return DNA.split('').reduce((sum, base) => sum + TRANSCRIPTION[base], '');
+}
