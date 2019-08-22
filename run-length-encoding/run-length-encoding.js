6f248d0879ed4ef8dfc6057b38b959d8593dcdd0
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-08-22 16:58:51 +0100

    1st complete solution

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -3,13 +3,27 @@ export const encode = (code) => {
     return code;
   }
   let counter = 1;
-  const CODESPLIT = code.split('');
-  return CODESPLIT.reduce((sum, letter, i) => {
-    letter === CODESPLIT[i + 1] ?
+  return [...code].reduce((solveCode, char, i) => {
+    char === [...code][i + 1] ?
       counter++ :
       counter === 1 ?
-      sum += letter :
-      (sum += '' + counter + letter, counter = 1);
-    return sum;
+      solveCode += char :
+      (solveCode += '' + counter + char, counter = 1);
+    return solveCode;
+  }, '')
+}
+
+export const decode = (code) => {
+  if (code === '') {
+    return code;
+  }
+  let repNumber = 0;
+  return [...code].reduce((encodeStr, char) => {
+    /\d/.test(char) === true ?
+      repNumber += char :
+      repNumber != 0 ?
+      (encodeStr += char.repeat(repNumber), repNumber = 0) :
+      encodeStr += char;
+    return encodeStr;
   }, '')
 }
