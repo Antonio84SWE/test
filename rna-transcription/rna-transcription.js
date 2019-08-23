08210e6e3e86dd0dbdafe45dbcab5acaea14f9d8
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-08-23 15:52:35 +0100

    refactoring

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,17 +1,7 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-const TRANSCRIPTION = {
-  A: 'U',
-  T: 'A',
-  C: 'G',
-  G: 'C',
-}
-
+const TRANSCRIPTION = new Map([['A', 'U'], ['T', 'A'], ['C', 'G'], ['G', 'C']])
 export const toRna = (DNA) => {
-  if(/[^ATCG]/gi.test(DNA)){
+  if (/[^ATCG]/gi.test(DNA)) {
     throw new Error('This DNA-sequence includes non-existing DNA-bases!');
   }
-  return DNA.split('').reduce((sum, base) => sum + TRANSCRIPTION[base], '');
+  return DNA.split('').reduce((RNAcode, DNAbase) => RNAcode += TRANSCRIPTION.get(DNAbase), '');
 }
