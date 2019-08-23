3cc76d81031f02f1eb70cb91675e352bf0ee3c18
Antonio Ascue Avalos <ascuecode@gmail.com>
2019-08-23 15:23:40 +0100

    refactoring

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,29 +1,13 @@
 export const encode = (code) => {
-  if (code === '') {
-    return code;
-  }
-  let counter = 1;
-  return [...code].reduce((solveCode, char, i) => {
-    char === [...code][i + 1] ?
-      counter++ :
-      counter === 1 ?
-      solveCode += char :
-      (solveCode += '' + counter + char, counter = 1);
-    return solveCode;
-  }, '')
+  return code === '' ? code :
+    code.match(/(.)\1*/gi)
+    .reduce((acc, curr) => curr.length === 1 ? acc += curr[0] :
+      acc += '' + curr.length + curr[0], '')
 }
 
 export const decode = (code) => {
-  if (code === '') {
-    return code;
-  }
-  let repNumber = 0;
-  return [...code].reduce((encodeStr, char) => {
-    /\d/.test(char) === true ?
-      repNumber += char :
-      repNumber != 0 ?
-      (encodeStr += char.repeat(repNumber), repNumber = 0) :
-      encodeStr += char;
-    return encodeStr;
-  }, '')
+  return code === '' ? code :
+    code.match(/(\d+)?./gi)
+    .reduce((acc, curr) => curr.length === 1 ? acc += curr.slice(-1) :
+    acc += curr.slice(-1).repeat(curr.slice(0,-1)), '')
 }
