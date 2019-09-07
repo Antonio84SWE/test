b715390c4cbd37f2a30b29e9ab0a0acb7764028b
Antonio Ascue <ascuecode@gmail.com>
2019-09-07 19:11:25 +0100

    making triplets with a given number of factors

--- pythagorean-triplet/pythagorean-triplet.js
+++ pythagorean-triplet/pythagorean-triplet.js
@@ -20,6 +20,22 @@ export class Triplet {
   }
 
   static where(operator) {
+    const max = operator.maxFactor || 100;
+    const min = operator.minFactor || 1;
+    const sum = operator.sum;
+    const triplets = [];
+    const maxB = Math.ceil(Math.sqrt(max ** 2 - min ** 2));
 
+    for (let a = min; a < maxB; a++) {
+      for (let b = a; b <= maxB; b++) {
+        for (let c = b; c <= max; c++) {
+          let triNumbers = new Triplet(a, b, c);
+          if (triNumbers.isPythagorean()) {
+            triplets.push(triNumbers);
+          }
+        }
+      }
+    }
+    return triplets
   }
 }
