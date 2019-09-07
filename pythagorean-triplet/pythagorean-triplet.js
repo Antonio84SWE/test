ad4ec620bc20b7bc79338dbbb6eb165dda40759e
Antonio Ascue <ascuecode@gmail.com>
2019-09-07 19:05:05 +0100

    upload

---
+++ pythagorean-triplet/pythagorean-triplet.js
@@ -0,0 +1,25 @@
+export class Triplet {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  sum() {
+    return this.a + this.b + this.c;
+  }
+
+  product() {
+    return this.a * this.b * this.c;
+  }
+
+  isPythagorean() {
+    return this.a ** 2 + this.b ** 2 === this.c ** 2 ?
+      true :
+      false
+  }
+
+  static where(operator) {
+
+  }
+}
