5651d7cdf443be8a09d2fd93a0ed172c3e54ec0d
Antonio Ascue <ascuecode@gmail.com>
2019-10-01 15:43:18 +0100

    empty DNA1-strand throws error

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -6,6 +6,9 @@
 export const compute = (DNA1, DNA2) => {
   const size1 = DNA1.length;
   const size2 = DNA2.length;
+  if (size1 === 0 && size2 !== 0) {
+    throw new Error('left strand must not be empty')
+  }
   if (size1 !== size2) {
     throw new Error('left and right strands must be of equal length')
   }
