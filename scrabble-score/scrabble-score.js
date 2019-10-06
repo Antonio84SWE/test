4a7fbdc4a54cefc847c4b1dd768fc371cc60c7dc
Antonio Ascue <ascuecode@gmail.com>
2019-10-06 13:14:52 +0100

    2nd refactoring -> from array to object-based score table

--- scrabble-score/scrabble-score.js
+++ scrabble-score/scrabble-score.js
@@ -1,18 +1,19 @@
-const SCORETABLE = [
-  ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 1],
-  ['D', 'G', 2],
-  ['B', 'C', 'M', 'P', 3],
-  ['F', 'H', 'V', 'W', 'Y', 4],
-  ['K', 5],
-  ['J', 'X', 8],
-  ['Q', 'Z', 10]
-];
+const SCORETABLE = {
+  'AEIOULNRST': 1,
+  'DG': 2,
+  'BCMP': 3,
+  'FHVWY': 4,
+  'K': 5,
+  'JX': 8,
+  'QZ': 10
+}
+
+const scoreTableProps = Object.keys(SCORETABLE);
 
 export const score = (word) => {
-  return [...word.toUpperCase()].reduce((sumScore, letter) => {
-    SCORETABLE.forEach(letterScore => letterScore.includes(letter) ?
-      sumScore += letterScore[letterScore.length - 1] :
-      sumScore);
-    return sumScore;
+  return [...word.toUpperCase()].reduce((score, letter) => {
+    scoreTableProps.forEach(letterGroup => letterGroup.includes(letter) ?
+      score += SCORETABLE[letterGroup] : score);
+    return score;
   }, 0);
-};
+}
