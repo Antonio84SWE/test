b2a3210399be696f934aef74b1c6c3270bae5b9e
Antonio Ascue <ascuecode@gmail.com>
2019-10-06 01:19:14 +0100

    refactoring

--- scrabble-score/scrabble-score.js
+++ scrabble-score/scrabble-score.js
@@ -1,26 +1,18 @@
-//
-// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const SCORETABLE = [
+  ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 1],
+  ['D', 'G', 2],
+  ['B', 'C', 'M', 'P', 3],
+  ['F', 'H', 'V', 'W', 'Y', 4],
+  ['K', 5],
+  ['J', 'X', 8],
+  ['Q', 'Z', 10]
+];
 
 export const score = (word) => {
-  const SCORETABLE = [
-    ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 1],
-    ['D', 'G', 2],
-    ['B', 'C', 'M', 'P', 3],
-    ['F', 'H', 'V', 'W', 'Y', 4],
-    ['K', 5],
-    ['J', 'X', 8],
-    ['Q', 'Z', 10]
-  ];
-  const letters = [...word.toUpperCase()];
-  let result = letters.reduce((sum, l) => {
-    SCORETABLE.forEach(lScore => {
-      if (lScore.includes(l)) {
-        sum = sum + lScore[lScore.length - 1];
-      }
-    });
-    return sum;
+  return [...word.toUpperCase()].reduce((sumScore, letter) => {
+    SCORETABLE.forEach(letterScore => letterScore.includes(letter) ?
+      sumScore += letterScore[letterScore.length - 1] :
+      sumScore);
+    return sumScore;
   }, 0);
-  return result;
 };
